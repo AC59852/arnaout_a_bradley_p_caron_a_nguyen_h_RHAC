50633cb73e6773d9e4f8b9078dcfc5b7eaaa2190
AC59852 <austin.caron1@gmail.com>
2020-01-21 11:34:35 -0500

    added boxed footer and nav for future use

---
+++ templates/footer.php
@@ -0,0 +1,9 @@
+<div>
+    <div class="footerSocial">
+        <img src="../public/images/social1" alt="Social Icon 1">
+        <img src="../public/images/social2" alt="Social Icon 2">
+        <img src="../public/images/social3" alt="Social Icon 3">
+        <img src="../public/images/social4" alt="Social Icon 4">
+    </div>
+    <h3>Copyright</h3>
+</div>
