7143701a36c78f9e50b07b42206b93a331d6da12
AC59852 <austin.caron1@gmail.com>
2020-02-24 17:36:02 -0500

    resizing

--- templates/header.php
+++ templates/header.php
@@ -7,12 +7,11 @@
   <div id="menu" class="">
   <nav class="main-nav">
     <ul>
-        <li><a class="navBtn" href="#hero">home</a></li>
+        <li><a class="navBtn" href="#help">help!</a></li>
         <li><a class="navBtn" href="#aboutSection">about us</a></li>
-        <li><a class="navBtn" href="#help">faq</a></li>
-        <li><a class="navBtn" href="#action">take action</a></li>
+        <li><a class="navBtn" href="#action">get involved</a></li>
         <li><a class="navBtn" href="#partners">partners</a></li>
-        <li><a class="navBtn" href="#home">contact</a></li>
+        <li><a class="navBtn" href="#home">donate</a></li>
     </ul>
   </nav>
 </div>
