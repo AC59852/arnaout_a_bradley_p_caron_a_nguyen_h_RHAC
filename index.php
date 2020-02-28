3408c19abd394482c382b354086f842687c69ebc
p-bradley <p_bradley@fanshaweonline.ca>
2020-02-28 02:42:16 -0500

    added favIcon

--- index.php
+++ index.php
@@ -12,6 +12,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/noframework.waypoints.min.js"></script>
+    <link rel="apple-touch-icon" sizes="180x180" href="public/favIcon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="public/favIcon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="public/favIcon/favicon-16x16.png">
+<link rel="manifest" href="public/favIcon/site.webmanifest">
+<link rel="mask-icon" href="public/favIcon/safari-pinned-tab.svg" color="#c10000">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
     <link rel="stylesheet" href="https://use.typekit.net/wvm1igw.css">
     <link rel="stylesheet" href="public/css/reset.css">
     <link rel="stylesheet" href="public/css/main.css">
